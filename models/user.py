2761ddf86a2ccfead75bc1e98914c73479f8804a
dantevlee <dante.leeseberg@gmail.com>
2024-02-19 16:12:36 -0600

    table issue

--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@ from db import db
 
 
 class UserModel(db.Model):
-    __tablename__ = "tags"
+    __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
